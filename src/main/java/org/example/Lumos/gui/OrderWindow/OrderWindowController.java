6163fee07dcce19779d90f1c08c15c7caf143d88
Kopikou <copilowa.nadezhda2015@yandex.ru>
2024-05-05 21:15:52 +0800

    Choosing artists

--- src/main/java/org/example/Lumos/gui/OrderWindow/OrderWindowController.java
+++ src/main/java/org/example/Lumos/gui/OrderWindow/OrderWindowController.java
@@ -1,10 +1,19 @@
 package org.example.Lumos.gui.OrderWindow;
 
+import org.example.Lumos.domain.entity.People;
+import org.example.Lumos.domain.entity.ShowProgram;
+import org.example.Lumos.hibernate.services.ShowProgramServiceImpl;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.List;
 
 public class OrderWindowController {
+    private List<JComboBox> artistsComboBoxes;
+    private ShowProgramServiceImpl showProgramService = new ShowProgramServiceImpl();
     public void execut(OrderWindowView orderWindowView){
         JFrame parentFrame = orderWindowView.getParentFrame();
         orderWindowView.addWindowListener(new WindowAdapter() {
@@ -14,5 +23,47 @@ public class OrderWindowController {
                 orderWindowView.dispose();
             }
         });
+
+        List<ShowProgram> showPrograms = showProgramService.findAllShowPrograms();
+        JComboBox showProgramComboBox = orderWindowView.getShowProgramComboBox();
+        for (int i = 0; i < showPrograms.size(); i++)
+            showProgramComboBox.addItem(showPrograms.get(i).getTitle());
+
+        artistsComboBoxes = orderWindowView.getArtistsComboBoxes();
+
+        showProgramComboBox.addActionListener(new SelectedShowProgramActionListener(showPrograms,showProgramComboBox));
+
+    }
+    private class SelectedShowProgramActionListener implements ActionListener{
+        private List<ShowProgram> showPrograms;
+        private JComboBox showProgramComboBox;
+        SelectedShowProgramActionListener(List<ShowProgram> showPrograms, JComboBox showProgramComboBox){
+            this.showPrograms = showPrograms;
+            this.showProgramComboBox = showProgramComboBox;
+        }
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int id = 0;
+            for (int i = 0; i < showPrograms.size(); i++){
+                if(showPrograms.get(i).getTitle() == showProgramComboBox.getSelectedItem())
+                    id = i;
+            }
+
+            int artictsCnt = showPrograms.get(id).getArtistsCnt();
+
+            for (int i = 0; i < artistsComboBoxes.size(); i++){
+                artistsComboBoxes.get(i).removeAllItems();
+                artistsComboBoxes.get(i).setEnabled(false);
+            }
+
+            for (int i = 0; i < showPrograms.get(id).getArtists().size(); i++){
+                for (int j = 0; j < artictsCnt; j++){
+                    artistsComboBoxes.get(j).addItem(showPrograms.get(id).getArtists().get(i).getName());
+                    artistsComboBoxes.get(j).setEnabled(true);
+                }
+            }
+
+
+        }
     }
 }
--- src/main/java/org/example/Lumos/gui/OrderWindow/OrderWindowView.java
+++ src/main/java/org/example/Lumos/gui/OrderWindow/OrderWindowView.java
@@ -1,22 +1,113 @@
 package org.example.Lumos.gui.OrderWindow;
 
+import org.jdesktop.swingx.JXDatePicker;
+
 import javax.swing.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class OrderWindowView extends JFrame {
     private JFrame parent;
+    private JPanel mainPanel;
+    private JPanel artistsPanel;
+    private JComboBox showProgramComboBox;
+    private JComboBox artistsComboBox1, artistsComboBox2, artistsComboBox3, artistsComboBox4, artistsComboBox5, artistsComboBox6, artistsComboBox7, artistsComboBox8;
+    private List<JComboBox> artistsComboBoxes;
+    private JComboBox techniciansComboBox1, techniciansComboBox2;
+    private JComboBox transferComboBox1,transferComboBox2;
+    private JLabel showProgramLabel;
+    private JLabel dateLabel;
+    private JLabel placeLabel;
+    private JLabel artitsLabel;
+    private JLabel techniciansLabel;
+    private JLabel transferLabel;
+    private JXDatePicker picker;
+    private JTextField placeTextField;
     public OrderWindowView(JFrame parent){
         super("Добавить заказ");
 
         this.parent = parent;
+
+        initComponents();
+        placeComponents();
 
         pack();
         setSize(1000,700);
         setResizable(false);
         setLocationRelativeTo(null);
         setVisible(true);
-        //setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
+
+    private void initComponents(){
+        mainPanel = new JPanel();
+        artistsPanel = new JPanel();
+
+        showProgramLabel = new JLabel("Шоу-программа");
+        showProgramComboBox = new JComboBox<>();
+
+        dateLabel = new JLabel("Дата");
+        picker = new JXDatePicker();
+        picker.setDate(Calendar.getInstance().getTime());
+        picker.setFormats(new SimpleDateFormat("dd.MM.yyyy"));
+
+        placeLabel = new JLabel("Место");
+        placeTextField = new JTextField(25);
+
+        artistsComboBox1 = new JComboBox<>();
+        artistsComboBox2 = new JComboBox<>();
+        artistsComboBox3 = new JComboBox<>();
+        artistsComboBox4 = new JComboBox<>();
+        artistsComboBox5 = new JComboBox<>();
+        artistsComboBox6 = new JComboBox<>();
+        artistsComboBox7 = new JComboBox<>();
+        artistsComboBox8 = new JComboBox<>();
+
+        artitsLabel = new JLabel("Артисты");
+        artistsComboBoxes = new ArrayList<>();
+        artistsComboBoxes.add(artistsComboBox1);
+        artistsComboBoxes.add(artistsComboBox2);
+        artistsComboBoxes.add(artistsComboBox3);
+        artistsComboBoxes.add(artistsComboBox4);
+        artistsComboBoxes.add(artistsComboBox5);
+        artistsComboBoxes.add(artistsComboBox6);
+        artistsComboBoxes.add(artistsComboBox7);
+        artistsComboBoxes.add(artistsComboBox8);
+    }
+    private void placeComponents(){
+        //mainPanel.setLayout(new BoxLayout(mainPanel,BoxLayout.X_AXIS));
+        artistsPanel.setLayout(new BoxLayout(artistsPanel,BoxLayout.Y_AXIS));
+
+        mainPanel.add(showProgramLabel);
+        mainPanel.add(showProgramComboBox);
+
+        mainPanel.add(dateLabel);
+        mainPanel.add(picker);
+
+        mainPanel.add(placeLabel);
+        mainPanel.add(placeTextField);
+
+        artistsPanel.add(artitsLabel);
+        artistsPanel.add(artistsComboBox1);
+        artistsPanel.add(artistsComboBox2);
+        artistsPanel.add(artistsComboBox3);
+        artistsPanel.add(artistsComboBox4);
+        artistsPanel.add(artistsComboBox5);
+        artistsPanel.add(artistsComboBox6);
+        artistsPanel.add(artistsComboBox7);
+        artistsPanel.add(artistsComboBox8);
+
+        mainPanel.add(artistsPanel);
+
+        this.add(mainPanel);
+    }
+
     public JFrame getParentFrame(){
         return this.parent;
+    }
+    public JComboBox getShowProgramComboBox(){return showProgramComboBox;}
+    public List<JComboBox> getArtistsComboBoxes(){
+        return artistsComboBoxes;
     }
 }
